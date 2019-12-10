f8a6024d971a1fa70ec8ec7753f9140cf53050b2
frederik <frede.madsen3@gmail.com>
2019-12-10 15:44:46 +0100

    pyspark doesn't work, jacob need halp

--- pysparkimage/app/quickexample.py
+++ pysparkimage/app/quickexample.py
@@ -1,22 +1,62 @@
 from __future__ import print_function
 
 import sys
+import re
 
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
+
+from pyspark import SparkConf,SparkContext
+from pyspark.sql import SparkSession, SQLContext
+from pyspark.sql.types import *
+
+
+schema = StructType([
+    StructField("Count", IntegerType()),
+    StructField("visitorId", StringType()),
+    StructField("visitNumber", IntegerType()),
+    StructField("visitId", StringType()),
+    StructField("visitStartTime", StringType()),
+    StructField("date", StringType()),
+    StructField("totals", StringType()),
+    StructField("trafficSource", StringType()),
+    StructField("device", StringType()),
+    StructField("geoNetwork", StringType()),
+    StructField("customDimensions", StringType()),
+    StructField("hits", StringType()),
+    StructField("fullVisitorId", StringType()),
+    StructField("userId", StringType()),
+    StructField("channelGrouping", StringType()),
+    StructField("socialEngagementType", StringType()),
+
+])
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: network_wordcount.py <hostname> <port>", file=sys.stderr)
-        exit(-1)
-    sc = SparkContext(appName="PythonStreamingNetworkWordCount")
-    ssc = StreamingContext(sc, 1)
 
-    lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
-    counts = lines.flatMap(lambda line: line.split(" "))\
-                  .map(lambda word: (word, 1))\
-                  .reduceByKey(lambda a, b: a+b)
-    counts.pprint()
+    spark = SparkSession \
+        .builder \
+        .appName("DSProjet") \
+        .getOrCreate()
 
-    ssc.start()
-    ssc.awaitTermination()
+    sqlContext = SQLContext(spark)
+
+    #conf = SparkConf().set('spark.driver.host', '127.0.0.1')
+    #sc = SparkContext(master='local', appName='DSProject', conf=conf)
+
+    #df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('Uber-Jan-Feb-FOIL.csv')
+
+    df = sqlContext.read.csv(
+        "/bigquerydata/ga_sessions_20160801.csv", header=True, schema=schema
+    )
+
+    #df.createOrReplaceTempView("data")
+    #df = spark.read.load("/bigquerydata/ga_sessions_20160801.csv",
+    #                 format="csv", sep=",", schema=schema, header="true")
+    df.show(n=2, truncate=True)
+
+    df.select("Count", "visitNumber").filter(df.visitNumber == 9).show()
+
+    #sqlContext.sql("SELECT visitNumber from data where visitNumber == 152").show()
+
+    #.filter(lambda line: len(line)<=16)
+    #res = sc.textFile("/bigquerydata/ga_sessions_20160801.csv").map(lambda line: re.sub(',(?=(?:[^"]*"[^"]*")*[^"]*$)', ';', line)).map(lambda line: line.split(";")).filter(lambda line: len(line) == 16)
+    #res.foreach(print)
+    #print(res)
